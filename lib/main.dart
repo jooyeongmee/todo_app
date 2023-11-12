478a4e965062bf4899a19fa580cad1e5feccde04
YINGMEI <j.yeongmee@gmail.com>
2023-11-12 15:41:42 +0900

    feat: firebase 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/create.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/model.dart';
 import 'package:todo_app/update.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -32,87 +39,108 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // 더미 데이터
-  List<TodoItem> todoList = [
-    TodoItem(content: "111", isDone: false),
-    TodoItem(content: "111", isDone: true),
-    TodoItem(content: "111", isDone: true),
-    TodoItem(content: "111", isDone: false),
-    TodoItem(content: "111", isDone: true)
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-            const Text(
-              "오늘의 todo",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 28),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('todo').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+            final documents = snapshot.data?.docs ?? []; // 문서들 가져오기
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("전체: ${todoList.length}"),
-                Text(
-                  "진행 중: ${todoList.where((element) => element.isDone == false).length}",
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                const Text(
+                  "오늘의 todo",
                   style: TextStyle(
-                      color: Colors.red[600], fontWeight: FontWeight.w500),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                Text(
-                    "완료: ${todoList.where((element) => element.isDone == true).length}"),
+                const SizedBox(height: 28),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text("전체: ${documents.length}"),
+                    Text(
+                      "진행 중: ${documents.where((doc) => doc.get('isDone') == false).length}",
+                      style: TextStyle(
+                          color: Colors.red[600], fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                        "완료: ${documents.where((doc) => doc.get('isDone') == true).length}"),
+                  ],
+                ),
+                const Divider(thickness: 1),
+                Container(
+                  constraints:
+                      const BoxConstraints(maxHeight: 400, minHeight: 400),
+                  child: documents.isNotEmpty
+                      ? ListView.builder(
+                          padding: const EdgeInsets.all(0),
+                          shrinkWrap: true,
+                          itemCount: documents.length,
+                          itemBuilder: (context, index) {
+                            final doc = documents[index];
+                            String content = doc.get('content');
+                            bool isDone = doc.get('isDone');
+                            return _buildTodoItem(
+                                TodoItem(content: content, isDone: isDone),
+                                doc.id);
+                          },
+                        )
+                      : const Center(child: Text('No items')),
+                ),
+                const SizedBox(height: 50),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final String todo = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Create()));
+                      if (todo.trim().isEmpty) return;
+                      TodoItem todoItem =
+                          TodoItem(content: todo, isDone: false);
+                      await FirebaseFirestore.instance.collection('todo').add({
+                        'content': todoItem.content, // 하고싶은 일
+                        'isDone': false, // 완료 여부
+                      });
+                    },
+                    child: const Text("추가"),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.1),
               ],
-            ),
-            const Divider(thickness: 1),
-            ListView.builder(
-              padding: const EdgeInsets.all(0),
-              shrinkWrap: true,
-              itemCount: todoList.length,
-              itemBuilder: (context, index) {
-                final TodoItem todoItem = todoList[index];
-                return _buildTodoItem(todoItem);
-              },
-            ),
-            const Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () async {
-                  final String todo = await Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Create()));
-                  if (todo.trim().isEmpty) return;
-                  setState(() {
-                    todoList.add(TodoItem(content: todo, isDone: false));
-                  });
-                },
-                child: const Text("추가"),
-              ),
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-          ],
+            );
+          },
         ),
       ),
     );
   }
 
-  Widget _buildTodoItem(TodoItem todoItem) {
+  Widget _buildTodoItem(TodoItem todoItem, String docId) {
     return Row(
       children: [
         Checkbox(
             value: todoItem.isDone,
-            onChanged: (value) {
-              setState(() {
-                todoItem.setIsDone();
-              });
+            onChanged: (value) async {
+              await FirebaseFirestore.instance
+                  .collection('todo')
+                  .doc(docId)
+                  .update({'isDone': value});
             }),
         const SizedBox(width: 12),
         Expanded(
@@ -127,19 +155,23 @@ class _MyHomePageState extends State<MyHomePage> {
             final String todo = await Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const Update()));
             if (todo.trim().isEmpty) return;
-            setState(() {
-              todoItem.setContent(todo);
+            setState(() async {
+              await FirebaseFirestore.instance
+                  .collection('todo')
+                  .doc(docId)
+                  .update({'content': todo});
             });
           },
         ),
         const SizedBox(width: 6),
         InkWell(
-          child: const Icon(Icons.delete_outline),
-          onTap: () {
-            setState(() {
-              todoList.removeWhere((element) => element == todoItem);
-            });
+          onTap: () async {
+            await FirebaseFirestore.instance
+                .collection('todo')
+                .doc(docId)
+                .delete();
           },
+          child: const Icon(Icons.delete_outline),
         ),
       ],
     );
